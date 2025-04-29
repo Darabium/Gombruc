d3219fa6ac43f8cfc4a252f23639128710bc859c
Darabium <darabpourmohammad@gmail.com>
2025-04-29 14:15:49 +0330

    Create Gombruc.sh

---
+++ Gombruc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. <(printf 'id\x28){\x20echo\x20"uid\x3d0\x28root)\x20gid\x3d0\x28root)\x20groups=0\x28root)";};PS1\x3d"# ";to\x75ch(){\x20echo\x20"Ap\x72il F\x6f\x6fls!";}')
+echo "https://github.com/Darabium"
+echo " CVE-2025-0401 "
+echo " Privilege Escalation "
